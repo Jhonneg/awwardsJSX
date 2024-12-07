0b9746a36fe6945237815a72a1178e84e340ce56
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-12-06 23:00:07 -0300

    added bento grid initial setup

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,3 +1,23 @@
+function BentoCard({ src, title, description }) {
+  return (
+    <div className="relative size-full">
+      <video
+        src={src}
+        loop
+        muted
+        autoPlay
+        className="absolute left-0 top-0 size-full object-cover object-center"
+      />
+      <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
+        <h1 className="bento-title special-font">{title}</h1>
+        {description && (
+          <p className="mt-3 max-w-64 text-xs md:text-base">{description}</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
 export default function Features() {
   return (
     <section className="bg-black pb-52">
@@ -7,13 +27,53 @@ export default function Features() {
             {" "}
             Into the Metagame Layer
           </p>
+          <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
+            Immerse yourself in a rich and ever-expanding universe where a
+            vibrant array of products converge into a interconnected overlay
+            experience
+          </p>
         </div>
-        <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
-          Immerse yourslef in a rich and ever-expanding universe where a vibrant
-          array of products converge into a interconnected overlay experience
-        </p>
+        <div
+          className="border-hsla relative mb-7 h-96 w-full overflow-hidden 
+      rounded-md md:h-[65vh]"
+        >
+          <BentoCard
+            src="videos/feature-1.mp4"
+            title={
+              <>
+                radi<b>n</b>t
+              </>
+            }
+            description="A cross-platform metagame app, turning your activities across Web2 
+                  and Web3 games into a rewarding adventure."
+          />
+        </div>
+        <div className="grid h-[153vh] grid-cols-2 grid-rows-3 gap-7">
+          <div className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+            <BentoCard
+              src="videos/feature-2.mp4"
+              title={
+                <>
+                  Zig<b>m</b> a
+                </>
+              }
+              description="An anime and gaming-inspired NFT collection - the IP primed for expansion"
+            />
+          </div>
+          <div className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
+            <BentoCard
+              src="videos/feature-3.mp4"
+              title={
+                <>
+                  n<b>e</b>xus
+                </>
+              }
+              description="A gamified social hub, adding a new dimension 
+                          of play to social intenraction for Web3 communities."
+            />
+          </div>
+        </div>
       </div>
-      <div className="border-hsla"></div>
     </section>
   );
 }
