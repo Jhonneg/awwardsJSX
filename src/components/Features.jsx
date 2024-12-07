1b225e4c47c8456fb30cea9a1ae2c7f120b1b44b
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-12-06 21:01:48 -0300

    started features section

---
+++ src/components/Features.jsx
@@ -0,0 +1,19 @@
+export default function Features() {
+  return (
+    <section className="bg-black pb-52">
+      <div className="container mx-auto px-3 md:px-10">
+        <div className="px-5 py-32">
+          <p className="font-circular-web text-lg text-blue-50">
+            {" "}
+            Into the Metagame Layer
+          </p>
+        </div>
+        <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
+          Immerse yourslef in a rich and ever-expanding universe where a vibrant
+          array of products converge into a interconnected overlay experience
+        </p>
+      </div>
+      <div className="border-hsla"></div>
+    </section>
+  );
+}
