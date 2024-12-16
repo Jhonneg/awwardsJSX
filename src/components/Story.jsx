fd88cfc745f42ac38ee6cc2158a2b12fddce4736
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-12-16 18:19:26 -0300

    added rounded corners component

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,11 +1,46 @@
 import { useRef } from "react";
 import AnimatedTitle from "./AnimatedTitle";
+import gsap from "gsap";
 
 export default function Story() {
-    const frameRef = useRef("null");
-    
+  const frameRef = useRef("null");
 
-    
+  function handleMouseLeave() {
+    const element = frameRef.current;
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX: 0,
+      rotateY: 0,
+      ease: "power1.inOut",
+    });
+  }
+
+  function handleMouseMove(e) {
+    const { clientX, clientY } = e;
+    const element = frameRef.current;
+
+    if (!element) return;
+
+    const rect = element.getBoundingClientRect();
+    const x = clientX - rect.left;
+    const y = clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((y - centerY) / centerY) * -10;
+    const rotateY = ((x - centerX) / centerX) * 10;
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX,
+      rotateY,
+      transformPerspective: 500,
+      ease: "power1.inOut",
+    });
+  }
+
   return (
     <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
       <div className="flex size-full flex-col items-center py-10 pb-24">
@@ -22,6 +57,10 @@ export default function Story() {
               <div className="story-img-content">
                 <img
                   ref={frameRef}
+                  onMouseLeave={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
+                  onMouseMove={handleMouseMove}
                   src="/img/entrance.webp"
                   alt="entrance"
                   className="object-contain"
