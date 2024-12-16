3d4bb5ed25c2664dad6827bc09ef70b5d9b30bb8
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-12-16 18:08:28 -0300

    initial story markup

---
+++ src/components/Story.jsx
@@ -0,0 +1,36 @@
+import { useRef } from "react";
+import AnimatedTitle from "./AnimatedTitle";
+
+export default function Story() {
+    const frameRef = useRef("null");
+    
+
+    
+  return (
+    <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+      <div className="flex size-full flex-col items-center py-10 pb-24">
+        <p className="font-general text-sm uppercase md:text-[10px]"></p>
+        the multiversal ip world
+        <div className="relative size-full">
+          <AnimatedTitle
+            title="The st<b>o</b>ry of <br /> a hidden real<b>m</b>"
+            sectionId="#story"
+            containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
+          />
+          <div className="story-img-container">
+            <div className="story-img-mask ">
+              <div className="story-img-content">
+                <img
+                  ref={frameRef}
+                  src="/img/entrance.webp"
+                  alt="entrance"
+                  className="object-contain"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
